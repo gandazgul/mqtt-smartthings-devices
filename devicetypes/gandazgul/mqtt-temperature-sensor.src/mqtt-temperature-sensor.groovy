b81f5e596a08a5e57b38a9d5de6a846e529558e2
Carlos Ravelo <ravelo.carlos@gmail.com>
2019-02-01 23:34:27 -0500

    indents tabs to spaces and clean up

--- devicetypes/gandazgul/mqtt-temperature-sensor.src/mqtt-temperature-sensor.groovy
+++ devicetypes/gandazgul/mqtt-temperature-sensor.src/mqtt-temperature-sensor.groovy
@@ -14,13 +14,13 @@
  *
  */
 metadata {
-	definition (name: "MQTT Temperature Sensor", namespace: "gandazgul", author: "Carlos Ravelo") {
-		capability "Temperature Measurement"
-		capability "Relative Humidity Measurement"
-		capability "Sensor"
-		capability "Health Check"
+    definition (name: "MQTT Temperature Sensor", namespace: "gandazgul", author: "Carlos Ravelo") {
+        capability "Temperature Measurement"
+        capability "Relative Humidity Measurement"
+        capability "Sensor"
+        capability "Health Check"
 
-		command "setTemperature", ["number"]
+        command "setTemperature", ["number"]
         command "setHumidityPercent", ["number"]
 
         command "markDeviceOnline"
@@ -28,48 +28,48 @@ metadata {
 
         command "setStatus"
 
-		fingerprint profileId: "0104", deviceId: "0302", inClusters: "0000,0001,0003,0009,0402,0405"
-	}
+        fingerprint profileId: "0104", deviceId: "0302", inClusters: "0000,0001,0003,0009,0402,0405"
+    }
 
-	// simulator metadata
-	simulator {
-		for (int i = 0; i <= 100; i += 10) {
-			status "${i}F": "temperature: $i F"
-		}
+    // simulator metadata
+    simulator {
+        for (int i = 0; i <= 100; i += 10) {
+            status "${i}F": "temperature: $i F"
+        }
 
-		for (int i = 0; i <= 100; i += 10) {
-			status "${i}%": "humidity: ${i}%"
-		}
-	}
+        for (int i = 0; i <= 100; i += 10) {
+            status "${i}%": "humidity: ${i}%"
+        }
+    }
 
-	// UI tile definitions
-	tiles {
-		valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state("temperature", label:'${currentValue}°',
-				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
-				]
-			)
-		}
+    // UI tile definitions
+    tiles {
+        valueTile("temperature", "device.temperature", width: 2, height: 2) {
+            state("temperature", label:'${currentValue}°',
+                backgroundColors:[
+                    [value: 31, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+                    [value: 74, color: "#44b621"],
+                    [value: 84, color: "#f1d801"],
+                    [value: 95, color: "#d04e00"],
+                    [value: 96, color: "#bc2323"]
+                ]
+            )
+        }
 
-		valueTile("humidity", "device.humidity") {
-			state "humidity", label:'${currentValue}%', unit:""
-		}
+        valueTile("humidity", "device.humidity") {
+            state "humidity", label:'${currentValue}%', unit:""
+        }
 
         valueTile("deviceHealth", "device.healthStatus", decoration: "flat", inactiveLabel: false) {
             state "online",  label: "ONLINE", backgroundColor: "#00A0DC", icon: "st.Health & Wellness.health9", defaultState: true
             state "offline", label: "OFFLINE", backgroundColor: "#E86D13", icon: "st.Health & Wellness.health9"
         }
 
-		main(["temperature", "humidity"])
-		details(["temperature", "humidity", "deviceHealth"])
-	}
+        main(["temperature", "humidity"])
+        details(["temperature", "humidity", "deviceHealth"])
+    }
 }
 
 // health checks
@@ -100,38 +100,6 @@ private setDeviceHealth(String healthState) {
     sendEvent(name: "healthStatus", value: healthState)
 }
 
-// Parse incoming device messages to generate events
-def parse(String description) {
-	def name = parseName(description)
-	def value = parseValue(description)
-	def unit = name == "temperature" ? getTemperatureScale() : (name == "humidity" ? "%" : null)
-	def result = createEvent(name: name, value: value, unit: unit)
-	log.debug "Parse returned ${result?.descriptionText}"
-
-	return result
-}
-
-private String parseName(String description) {
-	if (description?.startsWith("temperature: ")) {
-		return "temperature"
-	} else if (description?.startsWith("humidity: ")) {
-		return "humidity"
-	}
-	null
-}
-
-private String parseValue(String description) {
-	if (description?.startsWith("temperature: ")) {
-		return zigbee.parseHATemperatureValue(description, "temperature: ", getTemperatureScale())
-	} else if (description?.startsWith("humidity: ")) {
-		def pct = (description - "humidity: " - "%").trim()
-		if (pct.isNumber()) {
-			return Math.round(new BigDecimal(pct)).toString()
-		}
-	}
-	null
-}
-
 /**
  * Just mark the end of the execution in the log
  */
@@ -142,10 +110,7 @@ private void done() {
 def setStatus(type, status) {
     log.debug("Setting status ${type}: ${status}")
 
-    //if (type == "temp") {
-    //    setTemperature(Float.parseFloat(status));
-    //}
-    //else {
     sendEvent(name: type, value: status)
-    //}
+
+    done()
 }
